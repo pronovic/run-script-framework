9878039f7cf2013cb0f1f2638292b9b63956eafa
Kenneth J. Pronovici <pronovic@ieee.org>
2022-10-24 00:55:35 +0000

    Support the ability to not scan tests

--- .run/commands/pylint.sh
+++ .run/commands/pylint.sh
@@ -4,8 +4,24 @@
 # We generate relative paths in the output to make integration with Pycharm work better
 
 command_pylint() {
+   TESTS="tests"
+
+   while getopts "t" option; do
+     case $option in
+       t)
+         TESTS=""  # -t means to ignore the tests
+         ;;
+       ?)
+         echo "invalid option -$OPTARG"
+         exit 1
+         ;;
+     esac
+   done
+
+   shift $((OPTIND -1))  # pop off the options consumed by getopts
+
    echo "Running pylint checks..."
    TEMPLATE="{path}:{line}:{column} - {C} - ({symbol}) - {msg}"
-   poetry_run pylint --msg-template="$TEMPLATE" -j 0 "$@" $(ls -d src/*) tests
+   poetry_run pylint --msg-template="$TEMPLATE" -j 0 "$@" $(ls -d src/*) $TESTS
    echo "done"
 }
