9777e33ca8052ab4c2e98d622fa83878f777402f
Kenneth J. Pronovici <pronovic@ieee.org>
2022-12-30 08:22:21 +0000

    Update the release process

--- commands/tagrelease.sh
+++ commands/tagrelease.sh
@@ -1,5 +1,5 @@
 # vim: set ft=bash ts=3 sw=3 expandtab:
-# Update the changelog and tag a specific version of the code
+# Update the changelog, tag a specific version of the code, and push the changes
 
 command_tagrelease() {
    local VERSION DATE TAG FILES MESSAGE
@@ -12,8 +12,8 @@ command_tagrelease() {
    VERSION=$(echo "$1" | sed 's/^v//') # so you can use "0.1.5 or "v0.1.5"
    DATE=$(date +'%d %b %Y')
    TAG="v$VERSION" # follow PEP 440 naming convention
-   FILES="NOTICE pyproject.toml Changelog"
-   MESSAGE="Release v$VERSION to PyPI"
+   FILES="NOTICE Changelog"
+   MESSAGE="Release v$VERSION"
 
    if [ "$CURRENT_BRANCH" != "$DEFAULT_BRANCH" ]; then
       echo "*** You are not on $DEFAULT_BRANCH; you cannot release from this branch"
@@ -32,13 +32,6 @@ command_tagrelease() {
       exit 1
    fi
 
-   poetry version $VERSION
-   if [ $? != 0 ]; then
-      echo "*** Failed to update version"
-      exit 1
-   fi
-
-   run_command dos2unix pyproject.toml
    run_command sedreplace "s|^Version $VERSION[[:blank:]][[:blank:]]*unreleased|Version $VERSION     $DATE|g" Changelog
    run_command sedreplace "s|(^ *Copyright \(c\) *)([0-9,-]+)( *.*$)|\1$COPYRIGHT\3|" NOTICE
 
@@ -56,8 +49,14 @@ command_tagrelease() {
       exit 1
    fi
 
+   git push --follow-tags
+   if [ $? != 0 ]; then
+      echo "*** Push step failed"
+      exit 1
+   fi
+
    echo ""
-   echo "*** Version v$VERSION has been released and committed; you may publish now"
+   echo "*** Version v$VERSION has been tagged"
    echo ""
 }
 
